768cb99afa565bfab802d0b1075c66c1db017f59
ssahluwa <ssahluwa@ncsu.edu>
2018-07-29 08:43:47 -0400

    remove unncessary vars

--- main.go
+++ main.go
@@ -13,33 +13,6 @@ import (
 	"net/http"
 )
 
-// cliArgs defines the configuration that the CLI
-// expects. By using a struct we can very easily
-// aggregate them into an object and check what are
-// the expected types.
-// If we need to mock this later it's just a matter
-// of reusing the struct.
-type cliArgs struct {
-	Port int `arg:"-p" help:"port to listen to"`
-}
-
-var (
-	// args is a reference to an instantiation of
-	// the configuration that the CLI expects but
-	// with some values set.
-	// By setting some values in advance we provide
-	// default values that the user might provide
-	// or not.
-	args = &cliArgs{
-		Port: 8080,
-	}
-)
-
-// main performs the main routine of the application:
-//	1.	parses the args;
-//	2.	analyzes the declaration of the API
-//	3.	sets the implementation of the handlers
-//	4.	listens on the port we want
 func main() {
 
 	finishedChannel := make(chan bool)
